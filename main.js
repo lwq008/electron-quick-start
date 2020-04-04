c49b9dcc0988f556ed0d42ed62391ea3018c123a
lwq008 <liang.wenqi@gmail.com>
2020-04-04 18:06:41 +0800

    Enable NativeFileSystem blink feature.

--- main.js
+++ main.js
@@ -8,15 +8,20 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
+      devTools: true,
+      experimentalFeatures: true,
+      enableBlinkFeatures: "NativeFileSystem",
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  // mainWindow.loadFile('index.html')
+  // mainWindow.loadURL("https://googlechromelabs.github.io/text-editor/");
+  mainWindow.loadURL("http://localhost:8080/");
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
